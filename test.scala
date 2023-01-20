0336f8ddc5c303fab661547997aafced6eb3a247
Nisan <nnisan858@gmail.com>
2023-01-20 15:42:53 +0200

    new test

--- test.scala
+++ test.scala
@@ -6,7 +6,7 @@ class Home_Page extends Simulation {
   val scn = scenario("Home Page")
     .during(1 minute){
       exec(http("Home Page")
-        .get("http://localhost:8081/IgalNisanDanielNissim/"))
+        .get("http://192.168.31.116:8081/IgalNisanDanielNissim/"))
         .pause(5,10)
     }
   setUp(scn.inject(rampUsers(10) during(1 minutes)))
